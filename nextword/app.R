bdc6b112ead1e4152da3327486e540fce4217ee9
Christopher Boyd <Mikuana@gmail.com>
2017-04-06 00:07:27 -0400

    It worked.
    That was easier that I thought it would be. Shiny is pretty cool.

--- nextword/app.R
+++ nextword/app.R
@@ -2,50 +2,54 @@ library(htmltools)
 library(shiny)
 library(NextWordR)
 
-# Suggestion button module UI component
-ab_moduleUI = function(id){
-  ns = NS(id)
-  tagList(
-    fluidRow(
-      actionButton(ns("btn"), id) #,
-      # textOutput(ns("txt"))
-    )
-  )
+# Come up with a programmatic naming scheme to ensure consistent calls in
+# dynamic button generation and listeing
+sugg_butt_name = function(suggestion) { paste("sugg", suggestion, sep='_') }
+
+# Generate a single dynamic suggestion button
+sugg_butt_make = function(suggestion) {
+  actionButton(sugg_butt_name(suggestion), label=suggestion)
 }
 
-# Suggestion button module server component
-ab_module = function(input, output, session){
-  observeEvent(input$btn,{
-    output$txt = renderText("More information shown")
+# Generate a listener for a single dynamic suggestion button
+sugg_butt_listen = function(input, output, session, suggestion) {
+  observeEvent(input[[sugg_butt_name(suggestion)]],{
+    updateTextInput(
+      session, 'usertext',
+      value = paste(input$usertext, suggestion)
+    )
   })
 }
-
 
 ui =
   fluidPage(
     titlePanel("Next Word Text Prediction"),
       mainPanel(
         fluidRow(textInput('usertext', NULL, placeholder = "enter your text to see suggestions")),
-        uiOutput('buttons')
+        fluidRow(uiOutput('buttons'))
       )
     )
 
 
-server = function(input, output) {
+server = function(input, output, session) {
   ntext = eventReactive(input$goButton, {
     input$n
   })
 
+  # Watch for a change in user text
   observeEvent(input$usertext, {
-    suggestions = nextword(input$usertext)
+    suggestions = nextword(input$usertext)  # generate new suggestion based upon user text
 
-    output$buttons = renderUI({
+    output$buttons = renderUI({  # generate new buttons for suggestions
       div(
-        lapply(suggestions, ab_moduleUI)
+        lapply(suggestions, sugg_butt_make)
       )
     })
 
-    lapply(suggestions, function(x) callModule(ab_module, x))
+    lapply(suggestions, function(x) {  # generate listeners for new buttons
+      sugg_butt_listen(input, output, session, suggestion = x)
+    })
+
   })
 }
 
